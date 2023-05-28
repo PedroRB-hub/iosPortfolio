d79deb99a265d06197f6f2674fb7da7e54dabc38
Pedro R.B <108886158+PedroRB-hub@users.noreply.github.com>
2023-05-28 11:41:37 -0300

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,19 +1,18 @@
 function toggleDarkMode() {
-    if (document.documentElement.classList.contains("light")) {
+  if (document.documentElement.classList.contains("light")) {
       document.documentElement.classList.remove("light");
       document.documentElement.classList.add("dark");
-    } else if (document.documentElement.classList.contains("dark")) {
+  } else if (document.documentElement.classList.contains("dark")) {
       document.documentElement.classList.remove("dark");
       document.documentElement.classList.add("light");
-    } else {
+  } else {
       if (
-        window.matchMedia &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches
+          window.matchMedia &&
+          window.matchMedia("(prefers-color-scheme: dark)").matches
       ) {
-        document.documentElement.classList.add("dark");
+          document.documentElement.classList.add("dark");
       } else {
-        document.documentElement.classList.add("light");
+          document.documentElement.classList.add("light");
       }
-    }
   }
-
+}
